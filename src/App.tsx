2b2d798525273b2e25ed4ed17224bbfa5cc0a0cf
Vital9i <vitalikdoiniak@gmail.com>
2024-11-09 23:09:21 +0300

    lesson4_propstype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Accordion } from './components/Accordion/Accordion';
+import { Rating } from './components/Rating/Rating';
 
 function App() {
+  console.log('App rendering')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppTitle title={'this is component'}/>
+      <Rating value={2}/>
+      <Accordion titleValue={'Menu'} collapsed={false}/>
+      <Rating value={4}/>
+      <Accordion titleValue={'Users'} collapsed={false}/>
     </div>
   );
+}
+
+type AppTitlePropsType = {
+  title:string
+}
+
+export function AppTitle(props:AppTitlePropsType) {
+  console.log('AppTitle rendering')
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  )
 }
 
 export default App;
