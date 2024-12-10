e3ce7e47630813f6975697826c1904c083b9e914
Vital9i <vitalikdoiniak@gmail.com>
2024-12-10 16:25:35 +0300

    5 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Accordion } from './components/Accordion/Accordion';
-import { Rating } from './components/Rating/Rating';
+import {Accordion} from './components/Accordion/Accordion';
+import {Rating} from './components/Rating/Rating';
 
 function App() {
-  console.log('App rendering')
+    console.log('App rendering')
 
-  return (
-    <div className="App">
-      <AppTitle title={'this is component'}/>
-      <Rating value={2}/>
-      <Accordion titleValue={'Menu'} collapsed={false}/>
-      <Rating value={4}/>
-      <Accordion titleValue={'Users'} collapsed={false}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppTitle title={'this is component'}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Accordion titleValue={'Menu'} collapsed={true}/>
+            <Rating value={5}/>
+            <Rating value={6}/>
+            <Accordion titleValue={'Users'} collapsed={false}/>
+        </div>
+    );
 }
 
 type AppTitlePropsType = {
-  title:string
+    title: string
 }
 
-export function AppTitle(props:AppTitlePropsType) {
-  console.log('AppTitle rendering')
-  return (
-    <>
-      <h1>{props.title}</h1>
-    </>
-  )
+export function AppTitle(props: AppTitlePropsType) {
+    console.log('AppTitle rendering')
+    return (
+        <>
+            <h1>{props.title}</h1>
+        </>
+    )
 }
 
 export default App;
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,85 +1,28 @@
 import React from 'react';
 
 type RatingPropsType = {
-    value:number
+    value: number
 }
 
 export function Rating(props: RatingPropsType) {
     console.log('Rating rendering')
-
-    if (props.value === 1) {
-        return (
-            <div>
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if (props.value === 2) {
-        return (
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if (props.value === 3) {
-        return (
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if (props.value === 4) {
-        return (
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={false} />
-            </div>
-        )
-    }
-    if (props.value === 5) {
-        return (
-            <div>
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-                <Star selected={true} />
-            </div>
-        )
-    }
-
     return (
         <div>
-            <Star selected={false} />
-            <Star selected={false} />
-            <Star selected={false} />
-            <Star selected={false} />
-            <Star selected={false} />
+            {<Star selected={props.value > 0}/>}
+            {<Star selected={props.value > 1}/>}
+            {<Star selected={props.value > 2}/>}
+            {<Star selected={props.value > 3}/>}
+            {<Star selected={props.value > 4}/>}
+            {<Star selected={props.value > 5}/>}
         </div>
     )
-
 }
 
 type StarPropsType = {
-    selected:boolean
+    selected: boolean
 }
 
-function Star(props:StarPropsType) {
+function Star(props: StarPropsType) {
     console.log('Star rendering')
     if (props.selected === true) {
         return (
